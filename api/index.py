a38abce6223d96e08246b21203130fafc8c2675f
lucasltonieto <lucas_tonieto@sicredi.com.br>
2025-09-19 14:37:28 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,6 @@
 from app import app
 from vercel_wsgi import handle
 
-# A função que o Vercel executa
+# Ponto de entrada executado pelo Vercel
 def handler(request, context):
     return handle(app, request, context)
-
-
